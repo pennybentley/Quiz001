36f445dd7b2702253f659781ff8e1b3931957f7a
penny bentley <penny.bentley@cox.net>
2019-09-01 01:01:50 +0000

    Quiz 1 04-make page

--- app/controllers/quiz001s_controller.rb
+++ app/controllers/quiz001s_controller.rb
@@ -3,3 +3,45 @@ class Quiz001sController < ApplicationController
     @quiztab = Quiztab.first
   end
 end
+
+  class Card
+  attr_accessor :rank, :suit
+
+  def initialize(rank, suit)
+    self.rank = rank
+    self.suit = suit
+  end
+
+  def output_card
+    puts "#{self.rank} of #{self.suit}"
+  end
+
+  def self.random_card
+    Card.new(rand(10), :spades)
+  end
+end
+
+class Deck
+   
+    def initialize
+        @suits = ["Spades","Hearts","Diamonds","Clubs"]  
+        @ranks = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+        @cards = []
+        @suits.each do |suit|
+            @ranks.each do |rank|
+                @cards << Card.new(rank, suit)
+            end
+         end   
+         @cards.shuffle!
+         deck = Deck.new
+    end
+    
+    def deal
+        @cards.shift.output_card
+    end
+        
+end
+
+
+
+
